4402f91671bc418cd1df066c11f4f84bd8e24356
ckslcksl <cksdkrk1313@gmail.com>
2021-07-08 17:33:18 +0900

    초기 커밋.

---
+++ WpfApp3/App.xaml.cs
@@ -0,0 +1,81 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+
+            
+            var theme = new Theme("NewTheme2");
+            theme.AssemblyName = "DevExpress.Xpf.Themes.NewTheme2.v20.2";
+            Theme.RegisterTheme(theme);
+            ApplicationThemeHelper.ApplicationThemeName = "NewTheme2";
+            
+
+
+
+
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+            var splashScreen = new SplahWindow();
+            splashScreen.Topmost = true;
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+
+
+            Window wnd = null;
+            if (e.Args.Length > 2) {
+                if (e.Args[1].Equals("minus"))
+                {
+                    switch (e.Args[2])
+                    {
+                        // SBOM
+                        case "5000":
+                            wnd = new WindowSBOM(e.Args[3]);
+                            break;
+
+                        // SUPPLY
+                        case "6000":
+                            wnd = new WindowSupply(e.Args[3]);
+                            break;
+                    }
+                    if (wnd != null)
+                    {
+                        wnd.Left = -3000; // 초기에 안보이게
+                        wnd.WindowStyle = WindowStyle.None;
+                        wnd.Tag = e.Args[1];
+                        wnd.Title = "Window" + e.Args[2] ;
+                        wnd.ShowInTaskbar = false;
+                    }
+                }
+            } 
+            else if (wnd == null)
+            {
+                wnd = new WindowSBOM("MP20210415");
+                wnd.WindowState = WindowState.Maximized;
+                wnd.WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+
+            wnd.Show();
+            splashScreen.Close();
+
+        }
+    }
+}
---
+++ WpfApp3/WindowSupply.xaml.cs
@@ -0,0 +1,86 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Diagram;
+using System;
+using System.Linq;
+using System.Windows;
+using WpfApp3.Model;
+using System.Windows.Media;
+using DevExpress.Xpf.Grid;
+using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Xpf.Diagram.Themes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// WindowSupply.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class WindowSupply : BaseWindow
+    {
+        public WindowSupply(string versionNm)
+        {
+            SetVersionName(versionNm);
+
+            var theme = new Theme("NewTheme2");
+            theme.AssemblyName = "DevExpress.Xpf.Themes.NewTheme2.v20.2";
+            try
+            {
+                Theme.RegisterTheme(theme);
+            }
+            catch { }
+            ApplicationThemeHelper.ApplicationThemeName = "NewTheme2";
+            InitializeComponent();
+        }
+        void GenerateSupplyChartItem(object sender, DiagramGenerateItemEventArgs e)
+        {
+            SUPPLY s = (SUPPLY)e.DataObject;
+
+            string templateName = "";
+            int compareResult = DateTime.Compare(Convert.ToDateTime(s.AvailDate), Convert.ToDateTime(s.DemandDate));
+
+            switch (s.RoutingAction.ToString().Substring(0,1))
+            {
+                
+
+                case "S":
+                    templateName = "STOCK";
+                    dts.Stroke = (compareResult > 0) ? Brushes.Red : Brushes.DarkBlue ;
+                    break;
+                case "M":
+                    templateName = "MOVE";
+                    dtm.Stroke = (compareResult > 0) ? Brushes.Red : Brushes.Gray ;
+                    break;
+                case "B":
+                    templateName = "BUY";
+                    dtb.Stroke = (compareResult > 0) ? Brushes.Red : Brushes.DarkGreen ;
+                    break;
+                default:
+                    templateName = "PROD";
+                    dtp.Stroke = (compareResult > 0) ? Brushes.Red : Brushes.Goldenrod ;
+                    break;
+            }
+
+            e.Item = e.CreateItemFromTemplate(templateName);
+        }
+        private void OnPanelLoaded(object sender, RoutedEventArgs e)
+        {
+            var offset = GetPanelBottomRightOffset();
+            var location = diagramPanel.GetBounds(diagramPanel).BottomRight;
+            location.Offset(-panAndZoomGroup.FloatSize.Width - offset, -panAndZoomGroup.FloatSize.Height - offset);
+            panAndZoomGroup.FloatLocation = location;
+        }
+        double GetPanelBottomRightOffset()
+        {
+            var key = new DiagramDesignerControlThemeKeysExtension() { ResourceKey = DiagramDesignerControlThemeKeys.PanZoomBottomRightOffset };
+            return (double)diagram.FindResource(key);
+        }
+
+        private void grid1_PreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Type type = e.MouseDevice.Target.GetType();
+
+            DevExpress.Xpf.Grid.TableView tableView = (DevExpress.Xpf.Grid.TableView)e.Source;
+            spvm.SelectedSupplyNodeGroup = (SUPPLY)tableView.FocusedRowData.Row;
+            spvm.SelectedSupplyNode = (SUPPLY)tableView.FocusedRowData.Row;
+        }
+    }
+}
